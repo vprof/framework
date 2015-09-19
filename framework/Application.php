9fe300f33555fc2ff707378f90736591ccc58d6d
vprof <vprof@ukr.net>
2015-09-20 01:53:09 +0300

    Rout & Front

--- framework/Application.php
+++ framework/Application.php
@@ -2,20 +2,41 @@
 
 namespace Framework;
 
-use Framework\Router\Router;
-
 class Application {
-    
-	private $config;
-    
-    function __construct ($config) {
-	
-		$this->config = require_once($config);
+
+    private $config;
+
+    function __construct($config) {
+
+        $this->config = require_once($config);
         
     }
+
+    function run() {
+        $nRouter = $this->router();
+        $this->front($nRouter, $this->config['routes']);
+    }
     
-    function run(){
-		
-        $router = new Router($this->config['routes']);
+    function router(){
+        $uri = explode('/', $_SERVER['REQUEST_URI']);
+        if (!empty($uri[2])){
+            $uName = $uri[2];
+        }  else {
+            $uName = 'home';
+        }
+        return $uName;
+    }
+
+
+    function front ($router, $routes){
+        echo $router.'<br>';
+        var_dump($routes);
+        if(array_key_exists($router, $routes)){
+            $controller = $routes[$router]['controller'];
+            print_r($controller);
+            echo '<br>';
+            $action = $routes[$router]['action'];
+            print_r($action);
+        }
     }
 }
